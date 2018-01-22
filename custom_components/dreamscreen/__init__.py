a3f60c218a0c20eb1f6f8676635f5394e40d5ef4
GregoryDosh <GregoryDosh@users.noreply.github.com>
2018-01-22 02:38:00 -0600

    adding ambiance_color

--- custom_components/dreamscreen/__init__.py
+++ custom_components/dreamscreen/__init__.py
@@ -49,9 +49,24 @@ SERVICE_AMBIANCE_SCENE_SCHEMA = vol.Schema({
     vol.Required(CONF_AMBIANCE_SCENE): vol.All(int, vol.Range(min=0, max=8)),
 })
 
+
+SERVICE_AMBIANCE_COLOR = 'set_ambiance_color'
+CONF_AMBIANCE_COLOR_RED = 'red'
+CONF_AMBIANCE_COLOR_GREEN = 'green'
+CONF_AMBIANCE_COLOR_BLUE = 'blue'
+SERVICE_AMBIANCE_COLOR_SCHEMA = vol.Schema({
+    vol.Required(vol.Any(CONF_AMBIANCE_COLOR_RED,
+                         CONF_AMBIANCE_COLOR_GREEN,
+                         CONF_AMBIANCE_COLOR_BLUE),
+                 msg="Need at least 1 color specified."):
+    vol.All(int, vol.Range(min=0, max=255))
+})
+
+CONF_GROUP = 'group'
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
         vol.Required(CONF_IP_ADDRESS): cv.string,
+        vol.Optional(CONF_GROUP, default=0): int,
     })
 }, extra=vol.ALLOW_EXTRA)
 
@@ -130,7 +145,7 @@ class DreamScreen:
         """Set DreamScreen ambiance mode.
 
         Not sure on clamping/all values
-        0: Color/Slider?
+        0: RGB Color
         1: Scenes (Required for setting scene below)
         """
         mode = max(min(mode, 1), 0)
@@ -157,13 +172,26 @@ class DreamScreen:
         self._send_packet(packet)
         _LOGGER.debug("set scene {}".format(scene))
 
+    def set_ambiance_color(self,
+                           red: int=0,
+                           green: int=0,
+                           blue: int=0) -> None:
+        """Set DreamScreen ambient color."""
+        red = max(min(red, 255), 0)
+        green = max(min(green, 255), 0)
+        blue = max(min(blue, 255), 0)
+        packet = self._build_packet(5, [red, green, blue])
+        self._send_packet(packet)
+        _LOGGER.debug("set ambiance_color R{} G{} B{}".
+                      format(red, green, blue))
+
 
 @asyncio.coroutine
 def async_setup(hass, config):
     """Setup DreamScreen."""
     config = config.get(DOMAIN, {})
 
-    ds = DreamScreen(config[CONF_IP_ADDRESS])
+    ds = DreamScreen(ip=config[CONF_IP_ADDRESS], group=config[CONF_GROUP])
 
     hass.data[DOMAIN] = {
         DREAMSCREEN_CONTROLLER: ds
@@ -233,5 +261,21 @@ def async_setup(hass, config):
         SERVICE_AMBIANCE_SCENE,
         set_ambiance_scene,
         schema=SERVICE_AMBIANCE_SCENE_SCHEMA)
+
+    @asyncio.coroutine
+    def set_ambiance_color(call):
+        """Change DreamScreen Ambiance Color."""
+        red = call.data.get(CONF_AMBIANCE_COLOR_RED) or 0
+        green = call.data.get(CONF_AMBIANCE_COLOR_GREEN) or 0
+        blue = call.data.get(CONF_AMBIANCE_COLOR_BLUE) or 0
+        hass.data[DOMAIN][DREAMSCREEN_CONTROLLER]. \
+            set_ambiance_color(red=red, green=green, blue=blue)
+
+    # Ambiance Color Service
+    hass.services.async_register(
+        DOMAIN,
+        SERVICE_AMBIANCE_COLOR,
+        set_ambiance_color,
+        schema=SERVICE_AMBIANCE_COLOR_SCHEMA)
 
     return True
