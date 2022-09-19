eb8adea15c2352e3d0ece5cc0f9d4013668794f4
Kongxiao Zhao <freefrank21@gmail.com>
2022-09-18 21:19:49 -0600

    Update __init__.py
    Removed loop=hass.loop as asyncio.wait() no long accept loop since python 3.10

--- custom_components/dreamscreen/__init__.py
+++ custom_components/dreamscreen/__init__.py
@@ -173,7 +173,8 @@ async def async_setup(hass, config):
             updates.append(entity.async_update_ha_state(True))
 
         if updates:
-            await asyncio.wait(updates, loop=hass.loop)
+            """ Removed loop=hass.loop as asyncio.wait() no long accept loop since python 3.10"""
+            await asyncio.wait(updates)
 
     for service_name in SERVICE_TO_ATTRIBUTE:
         schema = SERVICE_TO_ATTRIBUTE[service_name].get("schema")
